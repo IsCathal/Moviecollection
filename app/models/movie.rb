83bb03a40235d17473aa88aa30227ed891adba0f
IsCathal <mccabemcathal@gmail.com>
2024-07-09 22:57:19 +0100

    added movie_genre table need to test with console

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   validates :title, presence: true,
                     length: { minimum: 3, maximum: 50 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_212629) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_214858) do
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_genres", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
