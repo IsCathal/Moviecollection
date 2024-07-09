12b9beebb387283e89f1d147545e66b19e8bd0f2
IsCathal <mccabemcathal@gmail.com>
2024-07-09 22:09:15 +0100

    added Most watched should refactor

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,24 @@ class Movie < ApplicationRecord
 
   validates :director, presence: true,
                        length: { minimum: 10, maximum: 300 }
+
+  def get_highest_count_of_views
+    movies = Movie.all
+
+    highest_views_number = 0
+
+    movies.each do |movie|
+      highest_views_number = movie.view_count if !movie.view_count.nil? && (movie.view_count > highest_views_number)
+    end
+
+    highest_views_number
+  end
+
+  def display_views_and_most_viewed
+    if view_count == get_highest_count_of_views && Movie.count > 1 && view_count != 0
+      "Views #{view_count} (Most Watched)"
+    else
+      "Views #{view_count}"
+    end
+  end
 end
