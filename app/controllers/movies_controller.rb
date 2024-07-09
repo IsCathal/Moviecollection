c8bbfc2cc1892879d19df964f74b974d455b5a77
IsCathal <mccabemcathal@gmail.com>
2024-07-09 19:57:52 +0100

    Add basic movie controller setup

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,32 @@
 class MoviesController < ApplicationController
   def show; end
 
-  def index; end
+  def index
+    @movies = Movie.all
+  end
 
-  def new; end
+  def new
+    @movie = Movie.all
+  end
 
   def edit; end
 
   def update; end
 
-  def create; end
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      # ADD MOVIE HERE
+      flash[:notice] = 'Movie was successfully created'
+      redirect_to root_path
+    else
+      flash[:notice] = 'There was an error'
+      render 'new'
+    end
+  end
+end
+
+def movie_params
+  params.require(:movie).permit(:title, :director, :release_year, :genres)
 end
